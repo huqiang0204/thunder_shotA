1077189c67b20832320eff57760801fdb4e4f23d
huqiang <huqiang1990@outlook.com>
2019-08-01 20:07:13 +0800

    fix

---
+++ Assets/Core/Editor/UIComposite/ShareImageEditor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ShareImageHelper), true)]
+[CanEditMultipleObjects]
+public class ShareImageEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        var help = target as ShareImageHelper;
+        EditorGUILayout.Space();
+        serializedObject.Update();
+        if (GUILayout.Button("AddTrans"))
+        {
+            help.AddSon();
+            help.Refresh();
+        }
+        if(help!=null)
+        {
+            var dat = help.datas;
+            for(int i=0;i<dat.Count;i++)
+            {
+                var v = EditorGUILayout.Vector3Field("LocalPosition", dat[i].localPosition);
+                dat[i].localPosition = v;
+                var a = EditorGUILayout.Vector3Field("LocalAngle", dat[i].Angle);
+                dat[i].Angle = a;
+                var s = EditorGUILayout.Vector3Field("LocalScale", dat[i].localScale);
+                dat[i].localScale = s;
+                var sd = EditorGUILayout.Vector2Field("SizeDelta", dat[i].sizeDelta);
+                dat[i].sizeDelta = sd;
+                var p = EditorGUILayout.Vector2Field("Pivot", dat[i].pivot);
+                dat[i].pivot = p;
+                var c = EditorGUILayout.ColorField( dat[i].color);
+                dat[i].color= c;
+                var o = EditorGUILayout.ObjectField(dat[i].sprite,typeof(Sprite),true);
+                dat[i].SetSprite(o as Sprite);
+                if (GUILayout.Button("Remove"))
+                {
+                    dat.RemoveAt(i);
+                    break;
+                }
+            }
+            help.Refresh();
+        }
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ Assets/Core/UIHelper/ShareImageHelper.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using huqiang.Data;
+using UGUI;
+using UnityEngine;
+
+public class ShareImageHelper : UICompositeHelp
+{
+    public class TransData
+    {
+        public Vector3 localPosition;
+        public Vector3 localScale=Vector3.one;
+        public Vector3 Angle;
+        public Vector2 sizeDelta=new Vector2(100,100);
+        public Vector2 pivot=new Vector2(0.5f,0.5f);
+        public Color color = Color.white;
+        UIVertex[] buff = new UIVertex[4];
+        public Sprite sprite;
+        public void SetSprite(Sprite sp)
+        {
+            if (sp == null)
+                return;
+            sprite = sp;
+            float tx = sprite.texture.width;
+            float ty = sprite.texture.height;
+            float x = sprite.rect.x / tx;
+            float y = sprite.rect.y / ty;
+            float w = sprite.rect.width;
+            float h = sprite.rect.height;
+            float r = x + w / tx;
+            float t = y + h / ty;
+            Vector2 p = sprite.pivot;
+            buff[0].uv0.x = x;
+            buff[0].uv0.y = y;
+            buff[1].uv0.x = x;
+            buff[1].uv0.y = t;
+            buff[2].uv0.x = r;
+            buff[2].uv0.y = t;
+            buff[3].uv0.x = r;
+            buff[3].uv0.y = y;
+        }
+        public UIVertex[] GetUVInfo()
+        {
+            float w = localScale.x * sizeDelta.x;
+            float h = localScale.y * sizeDelta.y;
+            var pos = localPosition;
+            float left = -pivot.x * w;
+            float right = (1 - pivot.x) * w;
+            float down = -pivot.y * h;
+            float top = (1 - pivot.y) * h;
+            buff[0].color = color;
+            buff[1].color = color;
+            buff[2].color = color;
+            buff[3].color = color;
+            var q = Quaternion.Euler(Angle);
+            buff[0].position = q * new Vector3(left, down) + pos;
+            buff[1].position = q * new Vector3(left, top) + pos;
+            buff[2].position = q * new Vector3(right, top) + pos;
+            buff[3].position = q * new Vector3(right, down) + pos;
+            return buff;
+        }
+        
+    }
+    public List<TransData> datas=new List<TransData>();
+    public override FakeStruct ToFakeStruct(DataBuffer data)
+    {
+        return base.ToFakeStruct(data);
+    }
+    void VertexCalculation(CustomRawImage raw)
+    {
+        var vert = new List<UIVertex>();
+        var tri = new List<int>();
+        int s = 0;
+        for (int i = 0; i < datas.Count; i++)
+        {
+            var son =datas[i];
+            if (son != null)
+            {
+                var uv = son.GetUVInfo();
+                vert.AddRange(uv);
+                tri.Add(s);
+                tri.Add(s + 1);
+                tri.Add(s + 2);
+                tri.Add(s + 2);
+                tri.Add(s + 3);
+                tri.Add(s);
+                s += 4;
+            }
+        }
+       
+        raw.uIVertices = vert;
+        raw.triangle = tri;
+        raw.SetVerticesDirty();
+    }
+    public void Refresh()
+    {
+        var raw = GetComponent<CustomRawImage>();
+        if (raw != null)
+            VertexCalculation(raw);
+    }
+    public void AddSon()
+    {
+        TransData trans = new TransData();
+        datas.Add(trans);
+    }
+}
