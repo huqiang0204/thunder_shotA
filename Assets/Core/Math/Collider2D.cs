7fe20c44cb71e78c0e8d2d2f5b7ac315f530657e
huqiang <huqiang1990@outlook.com>
2019-08-01 15:17:25 +0800

    fix

--- Assets/Core/Math/Collider2D.cs
+++ Assets/Core/Math/Collider2D.cs
@@ -4,26 +4,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace huqiang.Math
 {
     public class Collider2D
     {
-        protected enum ColliderType
+        public enum ColliderType
         {
             Dot, Line, Box, Circle, Edge, Polygon
         }
-        protected ColliderType Type;
-        public UITransform transform { get; private set; }
-        public Collider2D(UITransform trans)
+        public ColliderType Type;
+        public Vector2[] Points;
+        public float Radius;
+        public Vector2 Position;
+        public Coordinates coordinates;
+        public ModelElement transform { get; private set; }
+        public Collider2D(ModelElement trans)
         {
             transform = trans;
         }
         public virtual void Update()
         {
+            coordinates = ModelElement.GetGlobaInfo(transform);
+            Position = coordinates.Postion;
         }
-        public virtual void CheckCollision(Collider2D target)
+        public virtual bool CheckCollision(Collider2D target)
         {
+            return false;
         }
     }
 }
